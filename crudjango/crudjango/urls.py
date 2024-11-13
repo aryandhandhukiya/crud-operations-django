2cd9c621d56fa0232b799220a7267f30f0de58cf
Aryan <dhandhukiyaaryan05@gmail.com>
2024-11-14 00:21:19 +0530

    urls.py changes commmitted

--- crudjango/crudjango/urls.py
+++ crudjango/crudjango/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('crudapp/', include('crudapp.urls')),
+    path('', include('crudapp.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
